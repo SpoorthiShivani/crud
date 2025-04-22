5e4cf5ba82d502824d6554cd03c2c62ea5554ebc
SPOORTHI SHIVANI GADEGONI <spoorthi.shivani2002@gmail.com>
2025-04-22 10:08:45 +0000

    first commit

--- app/app/webapp/controller/View1.controller.js
+++ app/app/webapp/controller/View1.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/m/MessageToast"
-], (Controller, JSONModel, MessageToast) => {
+    "sap/m/MessageToast",
+    "sap/m/MessageBox"
+], (Controller, JSONModel, MessageToast, MessageBox) => {
     "use strict";
 
     return Controller.extend("app.controller.View1", {
@@ -11,9 +12,11 @@ sap.ui.define([
             var oModel = new JSONModel("odata/v4/my/employee");
             this.getView().setModel(oModel);
         },
+
         onCreateEmployee() {
             this.byId("createEmployeeDialog").open();
         },
+
         onConfirmCreateEmployee() {
             // Logic to create an employee
             var sID = this.byId("employeeID").getValue();
@@ -22,36 +25,56 @@ sap.ui.define([
             var sJobTitle = this.byId("employeeJobTitle").getValue();
             var sCompanyID = this.byId("employeeCompanyID").getValue();
             var sSalary = this.byId("employeeSalary").getValue();
- // Create the new employee object
- var oNewEmployee = {
-    ID: sID,
-    firstName: sFirstName,
-    lastName: sLastName,
-    jobTitle: sJobTitle,
-    companyId_ID: sCompanyID,
-    salary: sSalary
-};
 
-// Send a POST request to the CAP service to create the new employee
-$.ajax({
-    url: "/odata/v4/my/employee",
-    method: "POST",
-    contentType: "application/json",
-    data: JSON.stringify(oNewEmployee),
-    success: (data) => {
-        MessageToast.show("Employee created successfully!");
-        this.byId("createEmployeeDialog").close();
-        // Optionally, refresh the model to update the table
-        this.getView().getModel().refresh();
-    },
-    error: (error) => {
-        MessageToast.show("Error creating employee: " + error.responseText);
-    }
-});
-},
+            // Create the new employee object
+            var oNewEmployee = {
+                ID: sID,
+                firstName: sFirstName,
+                lastName: sLastName,
+                jobTitle: sJobTitle,
+                companyId_ID: sCompanyID,
+                salary: sSalary
+            };
+
+            // Send a POST request to the CAP service to create the new employee
+            $.ajax({
+                url: "/odata/v4/my/employee",
+                method: "POST",
+                contentType: "application/json",
+                data: JSON.stringify(oNewEmployee),
+                success: (data) => {
+                    MessageToast.show("Employee created successfully!");
+                    this.byId("createEmployeeDialog").close();
+                    
+                    // Optionally, refresh the model to update the table
+                    this.getView().getModel("mainModel").refresh();
+                },
+                error: (error) => {
+                    MessageToast.show("Error creating employee: " + error.responseText);
+                }
+            });
+        },
+
         onCancelCreateEmployee() {
             // Close the dialog without saving
             this.byId("createEmployeeDialog").close();
-        }
+        },
+
+        onDeletePress(oEvent) {
+            var oItem = oEvent.getSource().getParent();
+            var oContext = oItem.getBindingContext("mainModel");
+            var sID = oContext.getProperty("ID");
+            $.ajax({
+                url: "/odata/v4/my/employee(" + sID + ")",
+                method: "DELETE",
+                success: (data) => {
+                    MessageToast.show("Employee deleted successfully!");
+                    this.getView().getModel("mainModel").refresh();
+                },
+                error: (error) => {
+                    MessageToast.show("Error deleting employee: " + error.responseText);
+                }
+            });
+        },
     });
 });
